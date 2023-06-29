66a9b79f3418414b6c6acee052dcc38c9df36e65
OlgaStroeva <olga,stroevaa@gmail.com>
2023-06-29 21:22:09 +0300

    Task with SVG image

---
+++ Test.sh
@@ -0,0 +1,3 @@
+g++ main.cpp
+./a.out > Example1.svg < Example1.txt
+./a.out > Example2.svg < Example2.txt
--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-vector<double> input_numbers(size_t count) {
-  vector<double> result(count);
+vector<long double> input_numbers(size_t count) {
+  vector<long double> result(count);
   for (size_t i = 0; i < count; i++) {
     cin >> result[i];
   }
   return result;
 }
 
-void find_minmax(const vector<double>& numbers, double &min, double &max) {
+void find_minmax(const vector<long double>& numbers, long double &min, long double &max) {
     for (double number : numbers) {
         if (number < min) {
             min = number;
@@ -22,8 +22,8 @@ void find_minmax(const vector<double>& numbers, double &min, double &max) {
     }
 }
 
-vector<size_t> build_histogram(const vector<double>& numbers, const size_t bin_count){
-  double min = numbers[0], max = numbers[0];
+vector<size_t> build_histogram(const vector<long double>& numbers, const size_t bin_count){
+  long double min = numbers[0], max = numbers[0];
   find_minmax(numbers, min, max);
   vector<size_t> bins(bin_count);
   for (double number : numbers) {
@@ -36,10 +36,10 @@ vector<size_t> build_histogram(const vector<double>& numbers, const size_t bin_c
   return bins;
 }
 
-void show_histogram_text(const vector<size_t>& bins){
-  const size_t SCREEN_WIDTH = 80;
-  const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+const size_t SCREEN_WIDTH = 80;
+const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
 
+void show_histogram_text(const vector<size_t>& bins){
   size_t max_count = 0;
   for (size_t count : bins) {
       if (count > max_count) {
@@ -70,6 +70,38 @@ void show_histogram_text(const vector<size_t>& bins){
   }
 }
 
+const size_t HeightOfColumn = 25, Scale = 10; //scale = one char; border height = scale; space between columns = scale/2
+
+void draw_histogram_svg(const vector<size_t>& bins){
+  size_t max_count = 0, i = 0;
+  for (size_t count : bins) {
+      if (count > max_count) {
+          max_count = count;
+      }
+  }
+  const bool scaling_needed = max_count > MAX_ASTERISK;
+  const double scaling_factor = (double)MAX_ASTERISK / max_count;
+  const size_t bin_count = bins.size();
+  const char* colors[] = {"(0, 0, 255)", "(0, 255, 0)", "(255, 0, 0)", "(255, 0, 255)", "(255, 255, 0)", "(0, 255, 255)"};
+  //print start of svg
+  cout << "<?xml version='1.0' encoding='UTF-8' ?>\n<svg width='" << (SCREEN_WIDTH+5)*Scale << "' height='" //for example, if we don't heed "full screen" we can use (max_count+2)*Scale
+       << bin_count * (HeightOfColumn+Scale/2) + Scale*1.5 << "' viewBox='0 0 " << (SCREEN_WIDTH+5)*Scale << ' '
+       << bin_count * (HeightOfColumn+Scale/2) + Scale*1.5 << "' xmlns='http://www.w3.org/2000/svg'>\n";
+  //print all columns and text
+  for(auto bin : bins){
+      //Drawing text
+      cout << "<text text-anchor='end' x='" << Scale*4 << "' y='" << (HeightOfColumn+Scale/2)*(i+1) << "'><tspan font-weight='bold' font-size='"
+           << HeightOfColumn << "'>" << bin << "</tspan></text>\n";
+
+      //Drawing rectangle. Separated function isn't needed cause it's single line where we need all paramet
+    cout << "<rect x='" << Scale*5 << "' y='" << (HeightOfColumn+Scale/2)*i + Scale << "' width='" << (scaling_needed ?
+            ((size_t)(bin * scaling_factor)) : bin) * Scale <<
+            "' height='" << HeightOfColumn << "' style='fill:rgb" << colors[i%7] << "' />\n";
+    ++i;
+    }
+  cout << "</svg>";
+}
+
 int main() {
     // Ввод данных
     size_t number_count;
@@ -87,7 +119,8 @@ int main() {
     auto bins = build_histogram(numbers, bin_count);
 
     // Вывод данных
-    show_histogram_text(bins);
+    //show_histogram_text(bins);
+    draw_histogram_svg(bins);
 
     return 0;
 }
